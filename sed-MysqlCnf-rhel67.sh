9c37cec8d64a268f5e5dc6a96cc34b997836782b
Christopher Montes <lodotek@gmail.com>
2016-11-01 11:24:02 -0600

    Update sed-MysqlCnf-rhel67.sh

--- sed-MysqlCnf-rhel67.sh
+++ sed-MysqlCnf-rhel67.sh
@@ -6,7 +6,7 @@ if [ -w "$file" ] #Check if you have write permissions
 then
   echo "Setting default collation to UTF-8 in /etc/my.cnf" 
   sed '/\[mysqld\]/a # set default collation
-collation-server = utf8_unicode_ci\
+collation-server = utf8_unicode_ci
 init-connect='\''SET NAMES utf8'\''\
 character-set-server = utf8\' -i "$file"
 
